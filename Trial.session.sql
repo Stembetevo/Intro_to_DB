3218f3b474205c866f78b417276cb7576fd226d9
Stembetevo <stephenkaruru05@gmail.com>
2025-11-11 14:11:11 +0300

    Fix table name casing in SQL query and add initial data insertion for Graduates

---
+++ Trial.session.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO Graduates(
+    FirstName,
+    LastName,
+    Email,
+    EnrollmentDate
+)
+
+VALUES (
+    'Steve',
+    'Biko',
+    'stekakinyua10@gmail.com',
+    STR_TO_DATE('09-02-2025','%d-%m-%Y')
+)
